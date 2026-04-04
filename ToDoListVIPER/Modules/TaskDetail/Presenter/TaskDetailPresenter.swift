cbc7da9596a53d9ecb54091866abf2e030d5f5d0
amanbayserkeev0377 <amanbayserkeev0377@gmail.com>
2026-04-04 16:12:16 +0600

    Add TaskDetailProtocols, TaskDetailPresenter

---
+++ ToDoListVIPER/Modules/TaskDetail/Presenter/TaskDetailPresenter.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+final class TaskDetailPresenter {
+    
+    weak var view: TaskDetailViewProtocol?
+    var interactor: TaskDetailInteractorProtocol?
+    var router: TaskDetailRouterProtocol?
+    
+    private let mode: TaskDetailMode
+    
+    init(mode: TaskDetailMode) {
+        self.mode = mode
+    }
+}
+
+// MARK: - TaskDetailPresenterProtocol
+
+extension TaskDetailPresenter: TaskDetailPresenterProtocol {
+    
+    func viewDidLoad() {
+        if case .edit(let task) = mode {
+            view?.showTask(task)
+        }
+    }
+    
+    func didSaveTask(title: String, description: String) {
+        guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
+            view?.showError("Название задачи не может быть пустым")
+            return
+        }
+        
+        switch mode {
+        case .add:
+            interactor?.createTask(title: title, description: description)
+        case .edit(let task):
+            interactor?.updateTask(task, title: title, description: description)
+        }
+    }
+}
+
+// MARK: - TaskDetailInteractorOutputProtocol
+
+extension TaskDetailPresenter: TaskDetailInteractorOutputProtocol {
+    
+    func didSaveSuccessfully() {
+        DispatchQueue.main.async { [weak self] in
+            self?.router?.dismiss()
+        }
+    }
+    
+    func didFailWithError(_ error: any Error) {
+        DispatchQueue.main.async { [weak self] in
+            self?.router?.dismiss()
+        }
+    }
+}
--- ToDoListVIPER/Modules/TaskDetail/TaskDetailProtocols.swift
+++ ToDoListVIPER/Modules/TaskDetail/TaskDetailProtocols.swift
@@ -1,1 +1,35 @@
 import Foundation
+
+enum TaskDetailMode {
+    case add
+    case edit(ToDoTask)
+}
+
+// MARK: - View
+protocol TaskDetailViewProtocol: AnyObject {
+    func showTask(_ task: ToDoTask)
+    func showError(_ message: String)
+}
+
+// MARK: - Presenter
+protocol TaskDetailPresenterProtocol: AnyObject {
+    func viewDidLoad()
+    func didSaveTask(title: String, description: String)
+}
+
+// MARK: - Interactor
+protocol TaskDetailInteractorProtocol: AnyObject {
+    func createTask(title: String, description: String)
+    func updateTask(_ task: ToDoTask, title: String, description: String)
+}
+
+// MARK: - Interactor Output
+protocol TaskDetailInteractorOutputProtocol: AnyObject {
+    func didSaveSuccessfully()
+    func didFailWithError(_ error: Error)
+}
+
+// MARK: - Router
+protocol TaskDetailRouterProtocol: AnyObject {
+    func dismiss()
+}
